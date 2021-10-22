2ba56cfa3262a148b46fd891c9d3e6438c585c54
akhilshyam74 <89392108+akhilshyam74@users.noreply.github.com>
2021-10-22 16:27:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	fabric.image.fromURL('birthday_image.jpg')
+}
+
+function playSound(){
+	
+}
